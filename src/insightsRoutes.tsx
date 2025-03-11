6631f0232935550d019cd787675939fee32aa0f4
Laís Domiciano <161219781+laistdomiciano@users.noreply.github.com>
2025-03-11 07:48:35 +0100

    Update insightsRoutes.tsx

--- src/insightsRoutes.tsx
+++ src/insightsRoutes.tsx
@@ -1,7 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { appPostgresDb } from "./databases/db";
 
-// Define a type for the query result
 interface QueryResult {
   total: number;
 }
@@ -13,11 +12,8 @@ export async function insightsRoutes(app: FastifyInstance): Promise<void> {
       "SELECT COUNT(*) as total FROM data",
     );
 
-    // Type assertion to let TypeScript know what we expect
     const typedResult = result as unknown as QueryResult[];
 
-    // Return the result
-    //return reply.send({ total: String(typedResult[0].total) });
-    return reply.send({ total: typedResult[0].total });
+    return reply.send({ total: String(typedResult[0].total) });
   });
 }
