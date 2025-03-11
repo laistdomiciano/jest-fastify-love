5a8ae9adffde2fafc4a1ff4df88a99242847bf46
Laís Domiciano <161219781+laistdomiciano@users.noreply.github.com>
2025-03-11 07:35:31 +0100

    Update mathService.tsx

--- src/mathService.tsx
+++ src/mathService.tsx
@@ -1,9 +1,7 @@
-// Function we want to mock in tests
 export function getNumber(): number {
   throw new Error("Database connection failed!");
 }
 
-// Function we want to test that uses getNumber
 export function addToNumberFromDb(value: number): number {
   const dbNumber = getNumber();
   return value + dbNumber;
